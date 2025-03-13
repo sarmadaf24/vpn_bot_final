de226a2a2bd7752aef882e7d736e25a6b7a7d9a6
sarmadaf24 <sarmad.afzali@gmail.com>
2025-03-13 07:29:38 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+TOKEN = "7314186934:AAEN3XMuXkNqoDi9q-RWYU2hk2uHmKcPMLo"
+ZARINPAL_MERCHANT_ID = "a6820e30-eb4c-435c-a7c3-796cbb523e8c"
+
+NOWPAYMENTS_WALLET_USDT = "0xB40C230d3652679373d04Ca277BBCb25a4B309Be"
+NOWPAYMENTS_WALLET_TRON = "TBYWZW2rFWJY1rzCrM4yrX6ct25VWhFVEr"
+
+SERVERS = [
+    {"ip": "78.47.147.96", "domain": "", "ssl": False},
+    {"ip": "173.249.9.105", "domain": "sporti.footballonline.site", "ssl": True},
+    {"ip": "173.249.9.105", "domain": "varzeshi.footballonline.site", "ssl": True},
+]
+
